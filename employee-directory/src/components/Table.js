6890ab8aceb133a94d5e3b0ae4019690c8036734
Chee Quee <mitchellcq@gmail.com>
2021-01-13 16:56:02 +1100

    added custom hooks and more buttons to the table.js component

--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -4,26 +4,25 @@ import "./Table.css"
 import Button from "./Button.js"
 import { EmployeeContext } from './EmployeeContext.js';
 
-//Check out 20-State/03-Stu_useState
 function Table() {
-    // https://randomuser.me/documentation#howto
-    // exists in case I ever built functionality in to change the url, which would get a new set of employees. Currently not being used other than to store the url.
+
     const [url] = useState("https://randomuser.me/api/?results=10")
     // custom hook used for getting the employees from the api and storing the sort functions
     const { sortFunc } = useGet(url);
     // contains the employees to display in the table
-    const { displayedEmployees} = useContext(EmployeeContext)
+    const { displayedEmployees } = useContext(EmployeeContext)
 
     return (
         <table>
             <thead>
                 <tr>
-                    <td onClick={() => sortFunc("name")}><Button>First Name</Button></td>
-                    <td>Last Name</td>
-                    <td>UID</td>
+                    <td onClick={() => sortFunc("firstName")}><Button>First Name</Button></td>
+                    <td onClick={() => sortFunc("lastName")}><Button>Last Name</Button></td>
+                    <td onClick={() => sortFunc("userName")}><Button>Username</Button></td>
                     <td>Gender</td>
                     <td>E-mail</td>
                     <td onClick={() => sortFunc("age")}><Button>Age</Button></td>
+                    <td>Picture</td>
                 </tr>
             </thead>
             <tbody>
@@ -32,10 +31,11 @@ function Table() {
                         <tr key={employee.login.uuid}>
                             <td>{employee.name.first}</td>
                             <td>{employee.name.last}</td>
-                            <td>{employee.login.uuid}</td>
+                            <td>{employee.login.username}</td>
                             <td>{employee.gender}</td>
                             <td>{employee.email}</td>
                             <td>{employee.dob.age}</td>
+                            <td><img alt="user thumbnail" src={employee.picture.thumbnail} /></td>
                         </tr>
                     )
                 }
