49af634163720834940135244a6ce0aaae8cbb5b
Chee Quee <mitchellcq@gmail.com>
2021-01-14 15:41:01 +1100

    removed custom button component and added bootstrap for styling

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import Table from "./components/Table"
 import { EmployeeContext } from "./components/EmployeeContext"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [employees, setEmployees] = useState([]);
   const [displayedEmployees, setDisplayedEmployees] = useState([])
 
   return (
+
     <div className="App">
       <EmployeeContext.Provider value={{ employees, setEmployees, displayedEmployees, setDisplayedEmployees }}>
         <Navbar />
--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from 'react';
 import { useGet } from "../hooks/API.js"
 import "./Table.css"
-import Button from "./Button.js"
 import { EmployeeContext } from './EmployeeContext.js';
+import Button from 'react-bootstrap/Button';
 
 function Table() {
 
